ea6a2ea51a273faa491499d179356e88abdd3025
christopher <ing.sistemas.llanos@gmail.com>
2025-03-04 16:19:50 -0500

    Desarrollo del juego amigo secreto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # challenge-amigo-secreto_esp-main
+
+<h1 align="center"> Amigo secreto </h1>
--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []; //declaramos un vector vacio
+
+function agregarAmigo( ){
+
+    let name = document.querySelector(".input-name").value;
+
+    if (name != ""){
+        amigos.push(name);
+
+        let lista = document.getElementById("listaAmigos");
+        let nuevaLista = document.createElement("li");
+        nuevaLista.textContent= name;
+        lista.appendChild(nuevaLista);
+
+        document.querySelector(".input-name").value = "";
+    }
+    else{
+        alert("Por favor, inserte un nombre.");
+    }
+    
+}
+
+function sortearAmigo(){
+
+    if (amigos.length != 0)
+    {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSeleccionado = amigos[indiceAleatorio];
+
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = ` <p> Amigo elegido </p> ${amigoSeleccionado} `;
+    }
+    else
+    {
+        alert(" No se puede sortear");
+    }
+
+}
